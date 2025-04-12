4bf1483567977bd97416e331f20be1bdcefc2581
cc883015 <1044940343@qq.com>
2025-04-12 22:39:35 +1000

    Change App.js add index import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Login from './pages/Login';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Index />} />
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Index from './pages/Index';
 import Login from './pages/Login';
 
 function App() {
@@ -7,8 +26,11 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        {/* 首页路由，加载 Index 组件 */}
         <Route path="/" element={<Index />} />
+        {/* login page */}
         <Route path="/login" element={<Login />} />
+        {/* 可根据实际需求添加其他路由 */}
       </Routes>
     </Router>
   );
