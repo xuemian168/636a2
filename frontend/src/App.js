c71c7bb239f794527bbd88cda8717b310818da44
xuemian <xuemian888@gmail.com>
2025-04-12 20:34:02 +1000

    Remove unused routes from App component to streamline navigation and improve code clarity.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
-import Register from './pages/Register';
-import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
-import Index from './pages/Index';
-import Rooms from './pages/Rooms';
-import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -15,11 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
