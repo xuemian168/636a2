17825647fc8246c7e73d84367451161c69fec1f8
xuemian <xuemian888@gmail.com>
2025-05-16 09:06:38 +1000

    update baseurl

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
   // baseURL: 'http://52.65.208.68/api', // live
-  baseURL: http://13.239.47.75/api
+  baseURL: 'http://13.239.47.75/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
