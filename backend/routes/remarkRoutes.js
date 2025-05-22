16e68bd481256389954826186647bfde4d90eb26
xuemian <xuemian888@gmail.com>
2025-05-23 08:44:35 +1000

    feat: add isSellerorAdmin and isProviderorAdmin middleware for enhanced authorization

--- backend/routes/remarkRoutes.js
+++ backend/routes/remarkRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { protect, isSeller } from '../middleware/authMiddleware.js';
+import { protect, isSeller, isSellerorAdmin } from '../middleware/authMiddleware.js';
 import { createRemark, getAllRemarks, getRemarkById, updateRemark, deleteRemark } from '../controllers/remarkController.js';
 const router = express.Router();
 
-router.post('/', protect, isSeller, createRemark);
+router.post('/', protect, isSellerorAdmin, createRemark);
 router.get('/', protect, getAllRemarks);
 router.get('/:id', protect, getRemarkById);
 router.put('/:id', protect, isSeller, updateRemark);
